fefaaef2b03ed3b4322379b2942a7752073efdc7
Tim Blankemeyer <tblank73@uw.edu>
2016-05-16 10:11:19 -0700

    Add JS examples

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict'; //strict mode: catch silly errors
 
 //code goes here!
+/* My first program
+   (in JavaScript) */
+console.log("Hello world!"); //prints
+
+var x = 4;
+var y = x;
+x = 5
+
+console.log(x+', '+y);
+
+var str = "Hello world!";
+var res = str.toUpperCase();
+console.log(res);
